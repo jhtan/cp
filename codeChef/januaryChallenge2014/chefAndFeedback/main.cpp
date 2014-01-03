485dd4012c439d001067c66c2c68badcebe52e30
Jhonatan Castro <jhtan@jhtan.com>
2014-01-03 11:37:26 -0400

    Chef And Feedback problem AC from Codechef January Challenge

---
+++ codeChef/januaryChallenge2014/chefAndFeedback/main.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+typedef long long          ll;
+typedef vector<int>        vi;
+typedef pair<int, int>     ii;
+typedef vector<ii>         vii;
+typedef set<int>           si;
+typedef map<string, int>   msi;
+
+//#define for(i, a, b) \
+//  for(int i = int(a); i <= int(b); i++)
+//#define Rvi(c, it) \
+//  for(vi::iterator it = (c).begin(); it != (c).end(); it++)
+//#define Rvii(c, it) \
+//  for(vii::iterator it = (c).begin(); it != (c).end(); it++)
+//#define Rmsi(c, it) \
+//  for(msi::iterator it = (c).begin(); it != (c).end(); it++)
+
+int main() {
+  int t;
+  scanf("%d", &t);
+
+  while(t--) {
+    string s;
+    cin >> s;
+    bool sw = false;
+    
+    for(int i=1; i<s.length()-1; i++) {
+      if(s[i] != s[i-1] && s[i] != s[i+1]) {
+	sw = true;
+	break;
+      }
+    }
+
+    if(sw)
+      printf("Good\n");
+    else
+      printf("Bad\n");
+  }
+
+  return 0;
+}
