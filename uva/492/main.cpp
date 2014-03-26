bd36eff1de1464203b2adcf077eeec137b9f75a1
jhtan <jhtan@jhtan.com>
2014-03-26 00:01:25 -0400

    UVa 492, Pig-Latin, AC.

---
+++ uva/492/main.cpp
@@ -0,0 +1,65 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+typedef long long          ll;
+typedef vector<int>        vi;
+typedef pair<int, int>     ii;
+typedef vector<ii>         vii;
+typedef set<int>           si;
+typedef map<string, int>   msi;
+
+//#define for(i, a, b) \
+//  for(int i = int(a); i <= int(b); i++)
+//#define Rvi(c, it) \
+//  for(vi::iterator it = (c).begin(); it != (c).end(); it++)
+//#define Rvii(c, it) \
+//  for(vii::iterator it = (c).begin(); it != (c).end(); it++)
+//#define Rmsi(c, it) \
+//  for(msi::iterator it = (c).begin(); it != (c).end(); it++)
+
+int main() {
+  char x;
+  string s = "", t = "";
+
+  while((x = getchar()) != '\n') {
+    if((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')) {
+      s += x;
+    } else {
+      if(s.length()) {
+	if(s[0] == 'a' || s[0] == 'e' || s[0] == 'i' || s[0] == 'o' || s[0] == 'u' || s[0] == 'A' || s[0] == 'E' || s[0] == 'I' || s[0] == 'O' || s[0] == 'U') {
+	  s += "ay";
+	} else {
+	  s = s.substr(1) + s[0] + "ay";
+	}
+      }
+
+      t += s;
+      t += x;
+      s = "";
+    }
+  }
+
+  cout << t << endl;
+
+  return 0;
+}
