7a82c19ecbe6eca4285925e106106cc83c9c433c
jhtan <jhtan@jhtan.com>
2014-05-02 20:57:39 -0400

    CodeRoad Coding Contest, test 2, AC.

---
+++ codeRoadCodingContest/2014/test2/factorial1/main.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+typedef long long          ll;
+typedef vector<int>        vi;
+typedef pair<int, int>     ii;
+typedef vector<ii>         vii;
+typedef set<int>           si;
+typedef map<string, int>   msi;
+
+//#define for(i, a, b) \
+//  for(int i = int(a); i <= int(b); i++)
+//#define Rvi(c, it) \
+//  for(vi::iterator it = (c).begin(); it != (c).end(); it++)
+//#define Rvii(c, it) \
+//  for(vii::iterator it = (c).begin(); it != (c).end(); it++)
+//#define Rmsi(c, it) \
+//  for(msi::iterator it = (c).begin(); it != (c).end(); it++)
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  vector<int> F(20, 0);
+  F[0] = 1;
+  for(int i=1; i<20; i++)
+    F[i] = i * F[i-1];
+
+  while(n) {
+    printf("%d\n", F[n]);
+    scanf("%d", &n);
+  }
+
+  return 0;
+}
