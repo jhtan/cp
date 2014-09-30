e0b49de9be67d8281e421a06aa007115d212ad33
jhtan <jhtan@jhtan.com>
2014-09-29 23:47:28 -0400

    AbiertoDeInformatica, first contest.

---
+++ contests/abiertoInformatica2014/first/nombresGeniales/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef vector<int>        vi;
+typedef pair<int, int>     ii;
+typedef vector<ii>         vii;
+typedef set<int>           si;
+typedef map<string, int>   msi;
+
+int main() {
+  string s; cin >> s;
+  if(s.size() % 2 == 0)
+    printf("Es Genial\n");
+  else
+    printf("No es Genial\n");
+
+  return 0;
+}
