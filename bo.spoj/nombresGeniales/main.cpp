a7f4379f36e2591135bf8b0be51e07d17749f110
jhtan <jhtan@jhtan.com>
2014-10-21 22:09:09 -0400

    Bo SPOJ, Nombres Geniales. AC.

---
+++ bo.spoj/nombresGeniales/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef vector<int>        vi;
+typedef pair<int, int>     ii;
+typedef vector<ii>         vii;
+typedef set<int>           si;
+typedef map<string, int>   msi;
+
+int main() {
+  string s; cin >> s;
+  if(s.length() % 2 == 0) printf("Es Genial\n");
+  else printf("No es Genial\n");
+
+  return 0;
+}
