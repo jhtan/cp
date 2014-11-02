8c64c98ed464117a4434e4156970f61894dc51f0
jhtan <jhtan@jhtan.com>
2014-11-02 18:52:49 -0400

    Correction of Edit Distance Algorithm.

--- algorithms/dp/editDistance/main.cpp
+++ algorithms/dp/editDistance/main.cpp
@@ -2,22 +2,29 @@
 
 using namespace std;
 
+int M[1000][1000];
+
 int ed(string s, string t) {
-  if(!s.length() && !t.length()) return 0;
-  if(!s.length()) return t.length();
-  if(!t.length()) return s.length();
+  int m = s.length(), n = t.length();
 
-  int l = ed(s.substr(0, s.length() - 1), t) + 1;
-  int r = ed(s, t.substr(0, t.length() - 1)) + 1;
-  int c = ed(s.substr(0, s.length() - 1), t.substr(0, t.length() - 1)) + (s[s.length() - 1] != t[t.length() - 1]);
+  if(M[m][n] != -1) return M[m][n];
 
-  return min(l, min(r, c));
+  if(!m && !n) return M[m][n] = 0;
+  if(!m) return M[m][n] = n;
+  if(!n) return M[m][n] = m;
+
+  int l = ed(s.substr(0, m - 1), t) + 1;
+  int r = ed(s, t.substr(0, n - 1)) + 1;
+  int c = ed(s.substr(0, m - 1), t.substr(0, n - 1)) + (s[m - 1] != t[n - 1]);
+
+  return M[m][n] = min(l, min(r, c));
 }
 
 int main() {
   string s, t;
   cin >> s >> t;
-  
+
+  memset(M, -1, sizeof(M));
   cout << "Edit Distance = " << ed(s, t) << endl;
 
   return 0;
