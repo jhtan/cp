4a69c7850cef5fd5fd16100036078f489525a87a
jhtan <jhtan@jhtan.com>
2015-08-28 03:05:44 -0400

    Ladders, CF Round 4 div 2, A solved.

---
+++ codeForces/4div2/A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          ll;
+typedef vector<int>        vi;
+typedef pair<int, int>     ii;
+typedef vector<ii>         vii;
+typedef set<int>           si;
+typedef map<string, int>   msi;
+
+int main() {
+  int n; scanf("%d", &n);
+
+  (n == 2 || n & 1) ? printf("NO\n") : printf("YES\n");
+  
+  return 0;
+}
